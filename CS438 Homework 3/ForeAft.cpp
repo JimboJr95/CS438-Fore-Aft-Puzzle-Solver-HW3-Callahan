29d9da62c9f1f23f5f5f8c863c7fed986844b77b
jacalla <jacalla@siue.edu>
2018-02-09 15:14:09 -0600

    Added a few functions. Still need to add the search algorithms

--- CS438 Homework 3/ForeAft.cpp
+++ CS438 Homework 3/ForeAft.cpp
@@ -5,7 +5,7 @@
 // Due Date:	15 February 2018
 //
 // Description:
-//		This program is meant to solve a fore aft puzzle (example: https://www.cheesygames.com/foreaft/ )
+//		This program is meant to solve a fore aft puzzle given a board size (example: https://www.cheesygames.com/foreaft/ )
 //		It implements and A* search to solve. It may also use BFS and DFS, but these will only solve for small boards.
 //
 
@@ -59,6 +59,7 @@ void ForeAft::run() {
 		if (inputI == quit)
 			break;
 
+
 	board:;
 		// Get board size
 		if (inputI == 1)
@@ -82,9 +83,143 @@ void ForeAft::run() {
 			goto board;
 		}
 
+		boardSize = boardSizeI;
+
+		// Initialize the root node
+		root = new node(boardSizeI);
+		for (int i = 0; i < boardSizeI; i++)
+		{
+			for (int j = 0; j < boardSizeI; j++)
+			{
+				if (i == boardSize / 2 && j == boardSize / 2) {
+					root->arr[i][j] = blank;
+					root->blankR = i;
+					root->blankC = j;
+				}
+				else if ((i / (boardSizeI / 2 + 1)) == 0 && (j / (boardSizeI / 2 + 1)) == 0)
+					root->arr[i][j] = red;
+				else if ((i / (boardSizeI / 2)) >= 1 && (j / (boardSizeI / 2)) >= 1)
+					root->arr[i][j] = blue;
+			}
+		}
+
+
 		// Perform selected search on selected size
 
+		printBoard(root);
+
+		delete root;
 	}
+}
+
+void ForeAft::AStar() {
+
+}
+
+bool ForeAft::moveBlankUpOne(node *n) {
+	if (n->blankR - 1 >= 0 && n->arr[n->blankR - 1][n->blankC] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR - 1][n->blankC];
+		n->arr[n->blankR - 1][n->blankC] = blank;
+		--n->blankR;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankUpTwo(node *n) {
+	if (n->blankR - 2 >= 0 && n->arr[n->blankR - 2][n->blankC] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR - 2][n->blankC];
+		n->arr[n->blankR - 2][n->blankC] = blank;
+		n->blankR -= 2;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankDownOne(node *n) {
+	if (n->arr[n->blankR + 1][n->blankC] != invalid && n->blankR + 1 < boardSize)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR + 1][n->blankC];
+		n->arr[n->blankR + 1][n->blankC] = blank;
+		++n->blankR;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankDownTwo(node *n) {
+	if (n->blankR + 2 < boardSize && n->arr[n->blankR + 2][n->blankC] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR + 2][n->blankC];
+		n->arr[n->blankR + 2][n->blankC] = blank;
+		n->blankR += 2;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankLeftOne(node *n) {
+	if (n->blankC - 1 >= 0 && n->arr[n->blankR][n->blankC - 1] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR][n->blankC - 1];
+		n->arr[n->blankR][n->blankC - 1] = blank;
+		--n->blankC;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankLeftTwo(node *n) {
+	if (n->blankC - 2 >= 0 && n->arr[n->blankR][n->blankC - 2] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR][n->blankC - 2];
+		n->arr[n->blankR][n->blankC - 2] = blank;
+		n->blankC -= 2;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankRightOne(node *n) {
+	if (n->blankC + 1 < boardSize && n->arr[n->blankR][n->blankC + 1] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR][n->blankC + 1];
+		n->arr[n->blankR][n->blankC + 1] = blank;
+		++n->blankC;
+		return true;
+	}
+
+	return false;
+}
+
+bool ForeAft::moveBlankRightTwo(node *n) {
+	if (n->blankC + 2 < boardSize && n->arr[n->blankR][n->blankC + 2] != invalid)
+	{
+		n->arr[n->blankR][n->blankC] = n->arr[n->blankR][n->blankC + 2];
+		n->arr[n->blankR][n->blankC + 2] = blank;
+		n->blankC += 2;
+		return true;
+	}
+
+	return false;
+}
+
+// Copy board from source to target
+void ForeAft::copyNode(node *source, node *target) {
+	for (int i = 0; i < boardSize; i++)
+		for (int j = 0; j < boardSize; j++)
+			target->arr[i][j] = source->arr[i][j];
+	
+	target->blankR = source->blankR;
+	target->blankC = source->blankC;
 }
 
 // Checks to make sure the number input by the user is valid
@@ -93,4 +228,22 @@ bool ForeAft::validNumber(int num, int beginRange, int endRange) {
 		return true;
 	
 	return false;
+}
+
+// Print board to the command line
+void ForeAft::printBoard(node *n) {
+	for (int i = 0; i < boardSize; i++){
+		for (int j = 0; j < boardSize; j++) {
+
+			if (n->arr[i][j] == invalid)
+				cout << "# ";
+			else if (n->arr[i][j] == blank)
+				cout << "  ";
+			else if (n->arr[i][j] == red)
+				cout << "R ";
+			else
+				cout << "B ";
+		}
+		cout << endl;
+	}
 }
--- CS438 Homework 3/ForeAft.h
+++ CS438 Homework 3/ForeAft.h
@@ -11,6 +11,35 @@
 
 using namespace std;
 
+typedef int color;
+
+const color invalid = 99;
+const color red = 1;
+const color blue = 2;
+const color blank = 3;
+
+struct node {
+	// node constructor
+	color **arr;
+	int blankR, blankC;
+
+	node(int boardSize) {
+		arr = new color*[boardSize]; // initialize size of the array
+		blankR = 0;
+		blankC = 0;
+
+		// initialize second dimension of the array
+		for (int i = 0; i < boardSize; i++)
+			arr[i] = new color [boardSize];
+
+		// init array values to invalid
+		for (int i = 0; i < boardSize; i++)
+			for (int j = 0; j < boardSize; j++)
+				arr[i][j] = invalid;
+
+	}
+};
+
 class ForeAft
 {
 public:
@@ -22,6 +51,25 @@ public:
 	void run();
 
 private:
+	// Variables
+	node * root;
+	int boardSize;
+
+	// Functions
+	void AStar();
+
 	bool validNumber(int num, int beginRange, int endRange);
+	bool moveBlankUpOne(node *n);
+	bool moveBlankUpTwo(node *n);
+	bool moveBlankDownOne(node *n);
+	bool moveBlankDownTwo(node *n);
+	bool moveBlankLeftOne(node *n);
+	bool moveBlankLeftTwo(node *n);
+	bool moveBlankRightOne(node *n);
+	bool moveBlankRightTwo(node *n);
+
+	void copyNode(node *source, node *target);
+	void printBoard(node *n);
+	//bool isMoveable (direction dir, )
 };
 
